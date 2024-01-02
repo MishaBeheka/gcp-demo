fe12267749bc3f8b087203d428e8bdc1e3727820
Mykhailo_Beheka <Mykhailo_Beheka@epam.com>
2024-01-02 13:15:13 +0200

    Disable DB connection

--- src/main/java/com/gcp/finaltask/model/User.java
+++ src/main/java/com/gcp/finaltask/model/User.java
@@ -1,35 +1,35 @@
-package com.gcp.finaltask.model;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Entity
-@Table(name = "users")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString(onlyExplicitlyIncluded = true)
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "user_name", nullable = false)
-    @ToString.Include
-    private String userName;
-
-    @Column(name = "user_comment", nullable = false)
-    @ToString.Include
-    private String userComment;
-}
+//package com.gcp.finaltask.model;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//@Entity
+//@Table(name = "users")
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@ToString(onlyExplicitlyIncluded = true)
+//public class User {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(name = "user_name", nullable = false)
+//    @ToString.Include
+//    private String userName;
+//
+//    @Column(name = "user_comment", nullable = false)
+//    @ToString.Include
+//    private String userComment;
+//}
