c15dc56b9ca19307c083392d81b82e87561514fd
Mykhailo_Beheka <Mykhailo_Beheka@epam.com>
2024-01-14 14:18:58 +0200

    Connect to DB

--- src/main/java/com/gcp/finaltask/controller/HelloController.java
+++ src/main/java/com/gcp/finaltask/controller/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Hello from GCP final task!";
+        return "Hello from GCP final task with DB!";
     }
 }
